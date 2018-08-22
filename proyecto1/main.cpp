244ec8a20f692fd91a2cd55719be527e5c832e99
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-08-22 09:47:07 -0500

    short_int

--- proyecto1/main.cpp
+++ proyecto1/main.cpp
@@ -4,28 +4,15 @@ using namespace std;
 
 int main()
 {
-    int a;
-    int b;
-    int z;
-    cin >> a;
-    cin >> b;
-    z = a + b;
-    cout << "la suma de " <<a<< " y " <<b<< " es igual a " <<z << endl;
-
-    cout << "la multiplicacion de ";
-    cout << a;
-    cout << " y ";
-    cout << b;
-    cout << " es igual a ";
-    cout << (a*b)<< endl;
-
-    cout << "la divicion de ";
-    cout << a;
-    cout << " y ";
-    cout << b;
-    cout << " es igual a ";
-    cout << (a/b);
+    short int a;
+    short int z;
+    a=32766;
+    z=a+1;
+    cout <<z<< endl;
+    z=z+1;
+    cout <<z<< endl;
     return 0;
+
 }
 
 // sirve para comentar
