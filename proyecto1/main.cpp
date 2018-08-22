87e92e5e9171b717a6c646cf5dd66660d03c4d3a
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-08-22 09:26:41 -0500

    car

--- proyecto1/main.cpp
+++ proyecto1/main.cpp
@@ -11,6 +11,24 @@ int main()
     cin >> b;
     z = a + b;
     cout << "la suma de " <<a<< " y " <<b<< " es igual a " <<z << endl;
+
+    cout << "la multiplicacion de ";
+    cout << a;
+    cout << " y ";
+    cout << b;
+    cout << " es igual a ";
+    cout << (a*b)<< endl;
+
+    cout << "la divicion de ";
+    cout << a;
+    cout << " y ";
+    cout << b;
+    cout << " es igual a ";
+    cout << (a/b);
     return 0;
 }
 
+// sirve para comentar
+/*ee
+rer
+*/
