51b690f38a52dcc549e4e7e5c56257ce9bfc9e8d
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-08-29 09:06:03 -0500

    proyecto_1

--- proyecto1/main.cpp
+++ proyecto1/main.cpp
@@ -4,15 +4,33 @@ using namespace std;
 
 int main()
 {
-    short int a;
-    short int z;
-    a=32766;
-    z=a+1;
-    cout <<z<< endl;
-    z=z+1;
-    cout <<z<< endl;
-    return 0;
+    int a;
+    int b;
+    int c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cout <<a*b*c<< endl;
+    cout <<a+b+c<< endl;
+    cout <<(a+b+c)/3.0<< endl;
 
+    if(a>b && a>c){
+        cout <<"es:" <<a<< endl;}
+
+    else if(a<b && a<c && b>c){
+        cout <<"es:" <<b<< endl;}
+
+    else{}
+        cout <<"es:" <<c<< endl;
+
+
+    if(a%2==0)
+        cout <<a<< " es par"<< endl;
+    else
+        cout <<a<< " no es par"<< endl;
+
+    return 0;
+//escriba un programa que escriba un entero, si el valor ingresado es par e impar
 }
 
 // sirve para comentar
