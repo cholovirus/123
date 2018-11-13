0dc6f1ea91a06c19f68eb1b0331a6393b054fe1a
cholovirus <42578754+cholovirus@users.noreply.github.com>
2018-11-13 09:00:14 -0500

    Add files via upload

--- control/main.cpp
+++ control/main.cpp
@@ -2,20 +2,14 @@
 #include "Polygon.h"
 #include "Rectangulo.h"
 #include "Triangulo.h"
-#include "Circulo.h"
 #include "PolygonArray.h"
 using namespace std;
 
 int main()
 {
     Rectangulo a(4,5);
-    Rectangulo b(3,7);
-    Rectangulo c(4,7);
-    Rectangulo *z=&a;
-    z->printV();
-    a.area();
-    a.printPo();
-
+    Rectangulo b(2,4);
+    Rectangulo c(2,4);
     Rectangulo arr[]={a,b};
     int size =sizeof(arr)/sizeof(arr[0]);
     PolygonArray pa(arr,size);
