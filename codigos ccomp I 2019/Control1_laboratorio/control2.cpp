c4ef1d9bd1f5e31be0f0eb83f826602ebb2a1993
cholovirus <42578754+cholovirus@users.noreply.github.com>
2019-04-12 04:32:26 -0500

    Update control2.cpp

--- codigos ccomp I 2019/Control1_laboratorio/control2.cpp
+++ codigos ccomp I 2019/Control1_laboratorio/control2.cpp
@@ -12,17 +12,29 @@ void problem1(int x){
 }
 
 void problem2(int x){
-    float pi=0.0;
-    for(float i=1;i<x;i++){
-      if((i%2)){
-        pi+=4/i;
-        cout<<pi<<"--"<<i<<endl;
-      }
+    float pi=0;
+    int aux=1;
+    for(int i=1;i<=x;i++){
+        if(i%2){pi=pi+(4.0/aux);}
+        else{pi=pi-(4.0/aux);}
+        aux+=2;
+    }cout<<pi<<endl;
 
 }
-}
-//problem4
 
+int problem3(int x,int y){
+    if(x<y){
+        int temp=x;
+        x=y;
+        y=temp;
+
+    }
+    if(y==0){
+        return x;
+    }else{problem3(y,x%y);}
+}
+
+///problem4
 class Empleado{
 private:
     string nombre, apellido;
@@ -40,29 +52,23 @@ public:
     double getS(){return salario;}
 
 
+    void bajarSueldo(int s){
 
+        int x=(salario*s)/100;
+        salario=salario-x;
+    }
 
 
 };
 
-
-
-bool primo(int x){
-    int cont=0;
-    for(int i=1;i<=x;i++){
-        if(!(x%i)){cont++;}
-    }if(cont==2){return true;}
-    else{return false;}
-}
-
-long long problem5(long long x){
-
-    for(long long i=x;i>1;i--){
-        if(primo(i)){
-          if(x%i==0){return i; }
+void problem5(long long x){
+    int i;
+    for( i=2;1<x;i++){
+        while(!(x%i)){
+            x/=i;
         }
     }
-
+    cout<<i-1<<endl;
 }
 
 
@@ -71,14 +77,19 @@ int main()
     problem1(13);
     cout<<endl;
 
-    problem2(10);
+    problem2(1000);
+    cout<<endl;
 
+    cout<<problem3(28,40)<<endl;
+
+    ///problem4
     Empleado em1("pedro","alcantaro",3000);
     Empleado em2("Juan","ee",-500);
     cout<<em1.getN()<<em1.getA()<<em1.getS()<<endl;
     cout<<em2.getN()<<em2.getA()<<em2.getS()<<endl;
+    em1.bajarSueldo(10);
+    cout<<em1.getS()<<endl;
 
-    cout<<problem5(13195)<<endl;
-
+    problem5(600851475143);
     return 0;
 }
