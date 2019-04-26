6b5013733e706721139d4d7b98f62fef4e0e0950
cholovirus <42578754+cholovirus@users.noreply.github.com>
2019-04-26 10:21:17 -0500

    Update 41-50.cpp

--- codigos ccomp I 2019/Tarea libro parte 6/41-50.cpp
+++ codigos ccomp I 2019/Tarea libro parte 6/41-50.cpp
@@ -19,6 +19,45 @@ int libro_41(int x,int y){
 double libro_42(double x1, double y1,double x2, double y2){
     return sqrt(pow((x2-x1),2)+pow((y2-y1),2));
 }
+
+///libro_43 : error nunca acabaria la funcion por que main se iniciaria siempre
+/*
+int main()
+ {
+     int c = 0;
+
+     if ( ( c = cin.get() ) != EOF )
+         {
+         main();                        /// Error
+         cout << c;
+         }
+ }
+*/
+
+///libro_44: no tiene error de sintaxis
+/*
+int mystery( int, int ); // function prototype
+
+ int main()
+ {
+     int x = 0;
+     int y = 0;
+
+     cout << "Enter two integers: ";
+     cin >> x >> y;
+     cout << "The result is " << mystery( x, y ) << endl;
+ } // end main
+
+ // Parameter b must be a positive integer to prevent infinite recursion
+ int mystery( int a, int b )
+ {
+     if ( 1 == b ) // base case
+         return a;
+     else // recursion step
+          return a + mystery( a, b - 1 );
+ } // end function mystery
+
+*/
 int main()
 {
     cout<<libro_41(20,60);
@@ -26,5 +65,7 @@ int main()
 
     cout<<libro_42(2,3,3,2)<<endl;
     cout<<endl;
+
+
     return 0;
 }
