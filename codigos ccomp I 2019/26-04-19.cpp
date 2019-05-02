76678216aecb4b8a810e21b4c3a232ab12c267cc
cholovirus <42578754+cholovirus@users.noreply.github.com>
2019-05-02 11:05:12 -0500

    Update 26-04-19.cpp

--- codigos ccomp I 2019/26-04-19.cpp
+++ codigos ccomp I 2019/26-04-19.cpp
@@ -21,12 +21,12 @@ void imprimir(int arr[], int len){
 
 
 void burbuja(int arr[],int len){
-    for(int i=0;i<len-1;i++){
+    for(int i=0;i<len;i++){
         for(int j=1;j<len-1;j++){
-            if(arr[i]<arr[j]){
-                int temp= arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
+            if(arr[j>arr[j+1]){
+                int temp= arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
             }
         }
     }
