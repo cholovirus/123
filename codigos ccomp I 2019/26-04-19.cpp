bf411c893e357fdf80857fe1da67fa85406b2f11
cholovirus <42578754+cholovirus@users.noreply.github.com>
2019-05-02 19:03:40 -0500

    Update 26-04-19.cpp

--- codigos ccomp I 2019/26-04-19.cpp
+++ codigos ccomp I 2019/26-04-19.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
-#include <time.h>
-#include <math.h>
+
 using namespace std;
 
 
@@ -22,8 +21,9 @@ void imprimir(int arr[], int len){
 
 void burbuja(int arr[],int len){
     for(int i=0;i<len;i++){
-        for(int j=1;j<len-1;j++){
-            if(arr[j>arr[j+1]){
+        for(int j=0;j<len;j++){
+
+            if(arr[j]>arr[j+1]){
                 int temp= arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
@@ -32,8 +32,54 @@ void burbuja(int arr[],int len){
     }
 }
 
-void quicksort(){}
-void busquedaBinaria(){}
+void cambio(int *a, int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+int pivote(int arr[],int inicio, int fin){
+    int pivo=arr[fin];
+    int i=(inicio-1);
+
+    for (int j=inicio;j<=fin-1;j++){
+        if (arr[j] <= pivo){
+            i++;
+            cambio(&arr[i],&arr[j]);
+        }
+    }
+    cambio(&arr[i+1],&arr[fin]);
+    return (i+1);
+}
+
+void quickSort(int arr[], int inicio, int fin){
+    if(inicio<fin){
+        int piv=pivote(arr,inicio,fin);
+        quickSort(arr,inicio,piv-1);
+        quickSort(arr,piv+1,fin);
+    }
+
+}
+
+void busquedaBinaria(int arr[],int var){
+    int x=0, y=5,mit=0;
+    bool buscar=false;
+    while(x<=y){
+        mit=(x+y)/2;
+        if(arr[mit]==var){
+            buscar=true;
+            break;
+        }if(arr[mit]>var){
+            y=mit;
+            mit=(x+y)/2;
+        }if(arr[mit]<var){
+            x=mit;
+            mit=(x+y)/2;
+        }
+    }
+    if(buscar){cout<<"el numero "<<arr[mit]<<" esta en la posicion: "<<mit+1<<endl;}
+    else{cout<<"numero invalido"<<endl;}
+}
 
 bool primo(int numero){
     bool prim=true;
@@ -61,15 +107,25 @@ int cantidadPrimos(int arr[],int len){
 
 int main()
 {
-    int arr[]={100,7,99,3,65};
+    ///primer ejercicio
+    int arr[]={100,7,99,3,5};
     invertir(arr,5);
     imprimir(arr,5);
     cout<<endl;
 
+    ///segundo ejercicio
     burbuja(arr,5);
     imprimir(arr,5);
+    cout<<endl;
 
+    quickSort(arr,0,5-1);
+    imprimir(arr,5);
+    cout<<endl;
 
+    ///tercer ejercio
+    busquedaBinaria(arr,99);
+
+    ///cuarto ejercicio
     cout<<cantidadPrimos(arr,5);cout<<" primos en la lista"<<endl;
     return 0;
 }
